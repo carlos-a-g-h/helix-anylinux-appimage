242393848cdf835ba0ad76d0f2606921457d45f1
carlos-a-g-h <carloselputohabanero@proton.me>
2026-05-12 11:25:20 -0400

    Add files via upload

--- sfs_install.sh
+++ sfs_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# Installs the software inside the compressed AppDir to the system
+# Installs the software inside the compressed AppDir
 
 set -eux
 
 NAME="Helix"
 FILEPATH="$1"
 
-echo "Integrating: $NAME : $FILEPATH"
+echo "[RUN] $NAME : $FILEPATH"
 
 FILE_DESKTOP="helix.desktop"
 FILE_ICON="helix.png"
@@ -18,23 +18,38 @@ BIN_ABS="/usr/bin/hx"
 unsquashfs -f -d "$NAME" "$FILEPATH"
 
 # Move contents of the AppDir to the appimages directory
-mkdir -vp /usr/appimages/
+APPIMAGE_DIR="/usr/appimages/""$NAME"
+if [ -d "$APPIMAGE_DIR" ]
+then
+	rm -vrf "$APPIMAGE_DIR"
+	echo "[NOTICE] WIPED OUT: $APPIMAGE_DIR"
+else
+	mkdir -vp /usr/appimages/
+fi
 mv -vf "$NAME" /usr/appimages/
 
 # Link the main binary to the system
-ln -srf /usr/appimages/"$NAME"/"$BIN_APPDIR" "$BIN_ABS"
+ln -srf "$APPIMAGE_DIR"/"$BIN_APPDIR" "$BIN_ABS"
 
 # Copy desktop file
-cp -va /usr/appimages/"$NAME"/"$FILE_DESKTOP" /usr/share/applications/
+cp -va "$APPIMAGE_DIR"/"$FILE_DESKTOP" /usr/share/applications/
 
 # Copy icon file
-cp -va /usr/appimages/"$NAME"/"$FILE_ICON" /usr/share/icons/
+cp -va "$APPIMAGE_DIR"/"$FILE_ICON" /usr/share/icons/
 
-# Move the config
-mkdir -vp "$HOME"/.config/helix
-mv -vf /usr/appimages/"$NAME"/_config/* "$HOME"/.config/helix/
+# Move the config (and backup any old config)
+CONFIG_DIR="$HOME""/.config/helix"
+CONFIG_DIR_BAK="$HOME""/.config/helix.bak"
+if [ -d "$CONFIG_DIR" ]
+then	
+	mv -v "$CONFIG_DIR" "$CONFIG_DIR_BAK"
+	echo "[NOTICE] BACKED UP: $CONFIG_DIR_BAK"
+	mkdir -v "$CONFIG_DIR"
+fi
+mv -vf "$APPIMAGE_DIR"/_config/* "$CONFIG_DIR"/
 
 # Destroy the compressed AppDir file
 rm -v "$FILEPATH"
 
-echo "DONE"
+# ALl good
+echo "[OK] Installed: $NAME"
